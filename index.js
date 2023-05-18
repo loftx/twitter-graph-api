c680d8451e4ad0bb00785e72913f07918acf8261
Loftx <dev@loftx.co.uk>
2023-05-18 14:17:21 +0000

    Working tweets

--- index.js
+++ index.js
@@ -1,16 +1,105 @@
 module.exports = class TwitterGraphApi {
 
-  constructor(bearerToken, guestToken) {
+  constructor(bearerToken) {
+    this.twitterUrl = 'https://twitter.com/';
     this.twitterApiUrl = 'https://twitter.com/i/api/graphql/WzJjibAcDa-oCjCcLOotcg/';
 
-    this.axiosOptions = {
-      headers: {
-        'x-guest-token': guestToken,
-        'Authorization': 'Bearer ' + bearerToken
-      }
-    };
+    this.guestId = '';
+    this.guestToken = '';
+    this.bearerToken = bearerToken;
+    
+    this.axiosOptions = {};
   }
-  
+
+  getGuestId() {
+    return this.guestId;
+  }
+
+  setGuestId(guestId) {
+    this.guestId = guestId;
+  }
+
+  setGuestToken(guestToken) {
+    this.guestToken = guestToken;
+  }
+
+  getGuestToken() {
+    return this.guestToken;
+  }
+
+  setBearerToken(bearerToken) {
+    this.bearerToken = bearerToken;
+  }
+
+  getBearerToken() {
+    return this.bearerToken;
+  }
+
+  async getAuthGuestId() {
+    const axios = require('axios');
+
+    // twitter will try and redirect for the cookie - don't allow it and just get the response
+    const axiosOptions = {
+      maxRedirects: 0
+    }
+
+    try {
+      const response = await axios.get(this.twitterUrl, axiosOptions);
+    } catch (error) {
+
+      // error is thrown as it's a redirect so catch it and continue
+      
+      // TODO: check for existance of cookies
+      const cookieHeader = error.response.headers['set-cookie'][0];
+
+      const cookies = cookieHeader.split(/;\s*/).reduce((dict, cookie) => {
+        const [key, value] = cookie.split('=');
+        dict[key] = value;
+        return dict;
+      }, {});
+
+      // TODO: check for existance of guest_id
+
+      // TODO: check guestId is in form v1=xxxx after decoding
+
+      const guestId = cookies['guest_id'].substring(5);
+
+      return guestId;
+
+    }
+
+    throw 'Shouldn\'t happen';
+
+  }
+
+  async getAuthGuestToken(guestId) {
+    const axios = require('axios');
+
+    const axiosOptions = {
+      headers: {
+        'Cookie': 'guest_id=v1%3A' + guestId
+      }
+    }
+
+    var response = '';
+
+    try {
+      response = await axios.get(this.twitterUrl, axiosOptions);
+    } catch (error) {
+      // TODO: sort out errors
+      throw error;
+    }
+
+    const matches = response.data.match(/gt=([0-9]+)/);
+    if (matches && matches.length > 1) {
+      return matches[1];
+    } else {
+      // TODO: sort out errors
+      throw "No match found.";
+    }
+
+  }
+
   async getUserTweets(userId) {
 
     const axios = require('axios');
@@ -51,7 +140,14 @@ module.exports = class TwitterGraphApi {
 
     const queryString = 'UserTweets?variables=' + JSON.stringify(variables) + '&features=' + JSON.stringify(features);
 
-    const response = await axios.get(this.twitterApiUrl + queryString, this.axiosOptions).catch(error => {
+    const axiosOptions = {
+      headers: {
+        'x-guest-token': this.getGuestToken(),
+        'Authorization': 'Bearer ' + this.getBearerToken()
+      }
+    };
+
+    const response = await axios.get(this.twitterApiUrl + queryString, axiosOptions).catch(error => {
         console.log(error.response.status)
         return false;
     });
--- index.test.js
+++ index.test.js
@@ -1,16 +1,44 @@
 const twitterGraphApi = require('./index');
 
 describe('index.js', () => {
+  /* test('Check getAuthGuestId', async () => {
+    const twitter = new twitterGraphApi();
+    const guestId = await twitter.getAuthGuestId();
+
+    console.log(guestId);
+    expect(guestId).toMatch(/^\d+$/);
+
+  });
+
+  test('Check getAuthGuestToken', async () => {
+    const twitter = new twitterGraphApi();
+    const guestId = await twitter.getAuthGuestId();
+    const guestToken = await twitter.getAuthGuestToken(guestId);
+
+    console.log(guestToken);
+    expect(guestToken).toMatch(/^\d+$/);
+
+  }); */
+
   test('Check getUserTweets', async () => {
 
     const twitter = new twitterGraphApi(
-      process.env.TWITTER_BEARER_TOKEN,
-      process.env.TWITTER_GUEST_TOKEN
+      process.env.TWITTER_BEARER_TOKEN
     );
+
+    twitter.setGuestId(await twitter.getAuthGuestId());
+    twitter.setGuestToken(await twitter.getAuthGuestToken(twitter.getGuestId()));
 
     const result = await twitter.getUserTweets(process.env.TWITTER_USER_ID);
 
     console.log(result);
+
+    /* expect(result).toHaveProperty('data');
+    expect(result.data).toHaveProperty('me');
+    expect(result.data.me).toHaveProperty('personalizedFeed');
+    expect(result.data.me.personalizedFeed).toHaveProperty('feedItems');
+
+    console.log(result.data.me.personalizedFeed.feedItems); */
 
   });
 });
